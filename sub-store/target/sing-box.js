413941908c86e11b42cb699ce335fb086cee2a58
kumalum <github_nine@hotmail.com>
2026-01-22 01:27:24 +0800

    来源订阅名称，在andriod 下替换本地解析器的问题

--- sub-store/target/sing-box.js
+++ sub-store/target/sing-box.js
@@ -4,7 +4,7 @@
 let config = JSON.parse($files[0])
 const { type, name } = $arguments
 let proxies = await produceArtifact({
-    name: 'CDN',
+    name: 'Cloudflare',
     type: /^1$|col/i.test(type) ? 'collection' : 'subscription',
     platform: 'sing-box',
     produceType: 'internal',
@@ -145,7 +145,7 @@ function system_rule(headers) {
         config.route.override_android_vpn = true
         // 使用非 local 类型的dns 服务器 在安卓客户端会不工作
         // 将 alidns 解析器 替换为本地解析器
-        config.dns.servers[index_map.server_resolver] = complete_dns_servers_resolver
+        // config.dns.servers[index_map.server_resolver] = complete_dns_servers_resolver
     } else if (/windows|nt|mingw/i.test(ua)) {
         return
     }
